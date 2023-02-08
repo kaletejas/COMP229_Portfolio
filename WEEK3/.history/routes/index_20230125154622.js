6e703b44e1901565dea36ff405503f2e93f87ed2
kaletejas <tejaskale.messi@gmail.com>
2023-02-08 18:52:12 -0500

    initial commit

---
+++ WEEK3/.history/routes/index_20230125154622.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'P' });
+});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
