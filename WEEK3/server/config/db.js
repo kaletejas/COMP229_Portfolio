e4474c729c42fe8209cdbefb127d975eee111fdb
kaletejas <tejaskale.messi@gmail.com>
2023-03-10 00:04:30 -0500

    contacts updated

--- WEEK3/server/config/db.js
+++ WEEK3/server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI":"mongodb://localhost/contact_info"
+    //"URI":"mongodb://localhost/contact_info"
+    "URI" : "mongodb+srv://tejaskale:tejasmongo@cluster0.itzgoqx.mongodb.net/contact_info?retryWrites=true&w=majority" 
 }
